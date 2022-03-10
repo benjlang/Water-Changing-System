5ac0c2a7861883325cb29c5f322c1fece6620f8f
benjlang <40363793+benjlang@users.noreply.github.com>
2022-03-09 20:47:18 -0600

    added files

---
+++ changer_functions.c
@@ -0,0 +1,138 @@
+//Multiplexier pins for relay board
+const int selectPins[3] = {13, 15, 0};      // S0, S1, S2 | D7, D8, D3
+const int aOutput = 2;                      //D4
+
+const int dirty_float_leveler = A0;     //A0
+const int empty_dirty_water_pump = 0;
+
+struct tank {
+    int proximity_sensor;
+    int empty_float_level;
+    int offset;
+    int clean_pump;
+    int dirty_pump;
+};
+
+struct tank small_tank;
+struct tank large_tank;
+
+//set at default levels for first use
+int empty_float_level = 500; //0
+
+void set_float_level(struct tank* t) {
+    t->empty_float_level = analogRead(dirty_float_leveler);
+    EEPROM.put(t->offset, t->empty_float_level);
+    EEPROM.commit();
+}
+
+void set_tanks(struct tank* small, struct tank* large) {
+    large->clean_pump = 2;
+    large->dirty_pump = 4;
+    large->offset = 4;
+    EEPROM.get(large->offset, large->empty_float_level);
+    large->proximity_sensor = 5; //D1
+
+    small->clean_pump = 1;
+    small->dirty_pump = 3;
+    small->offset = 8;
+    EEPROM.get(small->offset, small->empty_float_level);
+    small->proximity_sensor = 4; //D2
+
+    EEPROM.get(0, empty_float_level);
+}
+
+/* Turns on a single relay block for a specified value
+ *  
+ * PARAM: item_id, value acossiated to relay block
+*/
+void turn_on_item(int item_id) {
+  selectMuxPin(item_id);
+  digitalWrite(aOutput, LOW);
+}
+
+/* Turns off a single relay block for a specified value
+ *  
+ * PARAM: item_id, value acossiated to relay block
+*/
+void turn_off_item(int item_id) {
+  selectMuxPin(item_id);
+  digitalWrite(aOutput, HIGH);
+}
+
+/* pumps clean water into tank */
+void pump_clean_in_tank(struct tank* t) {
+    selectMuxPin(t->clean_pump);
+    while(digitalRead(t->proximity_sensor) == LOW) {
+      digitalWrite(aOutput, LOW);
+      delay(500);
+    }
+    digitalWrite(aOutput, HIGH);
+}
+
+void pump_dirty_out_tank(struct tank* t) {
+    selectMuxPin(t->dirty_pump);
+    while (analogRead(dirty_float_leveler) > t->empty_float_level) {
+      digitalWrite(aOutput, LOW);
+      delay(500);
+    }
+    digitalWrite(aOutput, HIGH);
+}
+
+void perform_whole_change(struct tank* t) {
+    if (check_dirty_bucket) {
+        return;
+    }
+
+    pump_dirty_out_tank(t);
+    pump_clean_in_tank(t);
+    empty_dirty_bucket();
+
+    return;
+}
+
+/** checks if dirty bucket is full or not */
+bool check_dirty_bucket() {
+    Serial.println(analogRead(dirty_float_leveler));
+
+    if (analogRead(dirty_float_leveler) < empty_float_level) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/* Turns on the dirty emptying pump to empty dirty water */
+void empty_dirty_bucket() {
+    selectMuxPin(empty_dirty_water_pump);
+    while (check_dirty_bucket()) {
+      digitalWrite(aOutput, LOW);
+      delay(500);
+    }
+    digitalWrite(aOutput, HIGH);
+}
+
+/* The selectMuxPin function sets the S0, S1, and S2 pins to select the give pin
+ *  
+ * PARAM: pin to select
+ */
+void selectMuxPin(byte pin)
+{
+  if (pin > 7) return; // Exit if pin is out of scope
+  for (int i=0; i<3; i++)
+  {
+    if (pin & (1<<i))
+      digitalWrite(selectPins[i], HIGH);
+    else
+      digitalWrite(selectPins[i], LOW);
+  }
+}
+
+/* Sets all relays off first */
+void set_relays() {
+  for (int pin = 0; pin < 8; pin++) {
+    selectMuxPin(pin);
+    delay(100);
+    digitalWrite(aOutput, HIGH);
+    delay(100);
+  }
+}
---
+++ ir_changer_functions.c
@@ -0,0 +1,91 @@
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+#include <EEPROM.h>
+
+//ir remote sensor
+const uint16_t ir_sensor = 12;
+
+IRrecv irrecv(ir_sensor);
+decode_results results;
+
+extern const int selectPins[3]; // S0, S1, S2
+extern const int aOutput; // Connect common (Z) to 5 (PIM-capable)
+
+//Water sensors
+extern struct tank small;
+extern struct tank large;
+extern const int dirty_float_leveler;
+
+void setup() {
+    EEPROM.begin(12);
+    load_float_levels();
+    
+    set_tanks(&small, &large);
+
+    pinMode(large.proximity_sensor, INPUT);
+    pinMode(small.proximity_sensor, INPUT);
+    pinMode(ir_sensor, INPUT);
+    pinMode(dirty_float_leveler, INPUT);
+    
+    // Set up the select pins, as outputs
+    for (int i=0; i<3; i++)
+    {
+        pinMode(selectPins[i], OUTPUT);
+        digitalWrite(selectPins[i], LOW);
+    }
+    pinMode(aOutput, OUTPUT); // Set up Z as an output
+
+    set_relays();
+
+    irrecv.enableIRIn();  // Start the receiver
+    
+    Serial.begin(9600);
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    uint64_t number = results.value;
+    unsigned long long1 = (unsigned long)((number & 0xFFFF0000) >> 16 );
+    unsigned long long2 = (unsigned long)((number & 0x0000FFFF));
+
+    String hex = String(long1, HEX) + String(long2, HEX); // six octets
+  
+    Serial.println(hex);
+
+    if (results.value == 0x807f40bf) {
+      Serial.println("OK Pressed");
+
+      //turns all relays off
+      set_relays();
+    }
+    else if (results.value == 0x807fc03f) {
+      Serial.println("1 Pressed");
+      
+      water_change_small_tank();
+    }
+    else if (results.value == 0x807f827d) {
+      Serial.println("2 Pressed");
+      
+      water_change_large_tank();
+    }
+    else if (results.value == 0x807f609f) {
+      Serial.println("3 Pressed");
+      
+      turn_on_item(3);
+    }
+    else if (results.value == 0x807fa25d) {
+      Serial.println("4 Pressed");
+      
+      turn_on_item(4);
+    }
+    else if (results.value == 0x807f50af) {
+      Serial.println("0 Pressed");
+      
+      empty_dirty_bucket();
+    }
+    
+    delay(100);
+    irrecv.resume();  // Receive the next
+  }
+}
